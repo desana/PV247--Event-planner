379d640587042a5e2399efb322266fe5ea4819b7
xvitovs1 <409920@mail.muni.cz>
2016-10-23 20:06:07 +0200

    Add interface for VotesRepository

---
+++ EventPlanner/EventPlanner.Entities/Repositories/IVotesRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using EventPlanner.Entities;
+
+namespace EventPlanner.Repositories
+{
+    public interface IVotesRepository
+    {
+        Task<Vote> GetSingleVote(int id);
+
+        Task<IEnumerable<Vote>> GetAllVotes();
+
+        Task<Vote> AddVote(Vote vote);
+    }
+}
