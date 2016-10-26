bf59d34583f8ce5692ac8f9abcbe8d79e0f02f95
xvitovs1 <409920@mail.muni.cz>
2016-10-26 15:33:10 +0200

    Add EventPlannerInitializer

---
+++ EventPlanner/EventPlanner.Entities/EventPlannerInitializer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using EventPlanner.Entities;
+
+namespace EventPlanner
+{
+    class EventPlannerInitializer : System.Data.Entity.CreateDatabaseIfNotExists<EventPlannerDbContext>
+    {
+        protected override void Seed(EventPlannerDbContext context)
+        {
+            context.Events.Add(GetSomeEvent());
+            context.SaveChanges();
+        }
+
+        private static Event GetSomeEvent()
+        {
+            return new Event()
+            {
+                EventName = "Meeting",
+                EventDescription = "Company meeting winter 2016",
+                EventLink = "some.fake.link",
+                Votes = new List<Vote>
+                {
+                    new Vote
+                    {
+                        TimeAtPlace = new TimeAtPlace
+                        {
+                            Place = new Place { FourSquareLink = "https://foursquare.com/v/u-karla/4c1f3003b4e62d7fb244df93", Name = "U Karla"},
+                            Time = DateTime.Now
+                        }
+                    },
+                    new Vote
+                    {
+                        TimeAtPlace = new TimeAtPlace
+                        {
+                            Place = new Place { FourSquareLink = "https://foursquare.com/v/burger-inn/55a93496498e49f11b0a9532", Name = "Burger Inn"},
+                            Time = DateTime.Now
+                        }
+                    }
+                }
+               
+            };
+        }
+    }
+}
