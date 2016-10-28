9887208ad6483503f26a5b93dcabe791a78fc30a
Dax <410301@mail.muni.cz>
2016-10-28 17:36:10 +0200

    Changed EventPlanner framework version
    Changed EventPlanner framework version, added sample mvc controllers.

---
+++ EventPlanner/src/EventPlanner/Controllers/CreateAndVoteController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EventPlanner.Controllers
+{
+    public class CreateAndVoteController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return CreateAndVoteView();
+        }
+
+        private IActionResult CreateAndVoteView()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
