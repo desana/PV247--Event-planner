f98a8744e1dc1e8693f9b1110be3e93a73760ef5
xvitovs1 <409920@mail.muni.cz>
2016-11-01 21:34:42 +0100

    Add bootstrap to views

--- EventPlanner/EventPlanner/wwwroot/js/AddPlacesAndTimes.jsx
+++ EventPlanner/EventPlanner/wwwroot/js/AddPlacesAndTimes.jsx
@@ -1,37 +1,36 @@
 ﻿class PlacesApp extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.state = {items: [], text: ''};
-  }
+    constructor(props) {
+        super(props);
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.state = { items: [], text: '' };
+    }
 
-  render() {
-    return (
+    render() {
+        return (
       <div>
-        <form onSubmit={this.handleSubmit}>
-          <input onChange={this.handleChange} value={this.state.text} />
-          <button>{'Add another place'}</button>
-        </form>
+          <label htmlFor="fieldPlaces">Places</label>
+          <input className="form-control" id="fieldPlaces" onChange={this.handleChange} value={this.state.text} />
+          <button className="btn btn-default">{'Add another place'}</button>
       </div>
     );
-  }
+    }
 
-  handleChange(e) {
-    this.setState({text: e.target.value});
-  }
+    handleChange(e) {
+        this.setState({ text: e.target.value });
+    }
 
-  handleSubmit(e) {
-    e.preventDefault();
-    var newItem = {
-      text: this.state.text,
-      id: Date.now()
-    };
-    this.setState((prevState) => ({
-      items: prevState.items.concat(newItem),
-      text: ''
-    }));
-  }
+    handleSubmit(e) {
+        e.preventDefault();
+        var newItem = {
+            text: this.state.text,
+            id: Date.now()
+        };
+        this.setState((prevState) => ({
+            items: prevState.items.concat(newItem),
+            text: ''
+        }));
+    }
 }
 
 ReactDOM.render(<PlacesApp />, places);
