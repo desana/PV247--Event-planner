b57021b072974f0edb3055c8d5b53883c7eee34b
Jana Morozová <morozova.jana@gmail.com>
2016-11-06 19:08:48 +0100

    fixed broken solution

---
+++ EventPlanner/Services/Event/EventService.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using AutoMapper;
+using EventPlanner.Repositories;
+
+namespace EventPlanner.Services.Event
+{
+    public class EventService : IEventService
+    {
+        private readonly IMapper _mapper;
+        private readonly IEventsRepository _eventRepository;
+
+        public EventService(IMapper mapper, IEventsRepository eventRepository)
+        {
+            _mapper = mapper;
+            _eventRepository = eventRepository;
+        }
+
+        public async Task<IEnumerable<DataTransferModels.Event>> GetAllEvents()
+        {
+            IEnumerable<Entities.Event> dataAccessEventModel = await _eventRepository.GetAllEvents();
+            return _mapper.Map<IEnumerable<Entities.Event>, IEnumerable<DataTransferModels.Event>>(dataAccessEventModel);
+        }
+
+        public async Task<DataTransferModels.Event> AddEvent(DataTransferModels.Event newEvent)
+        {
+            newEvent.GenerateUniqueLink();
+
+            Entities.Event dataAccessEventModel = _mapper.Map<Entities.Event>(newEvent);
+            dataAccessEventModel = await _eventRepository.AddEvent(dataAccessEventModel);
+
+            return _mapper.Map<DataTransferModels.Event>(dataAccessEventModel);
+        }
+
+        public async Task<DataTransferModels.Event> GetSingleEvent(string eventName)
+        {
+            Entities.Event dataAccessEventModel = await _eventRepository.GetSingleEvent(eventName);
+            return _mapper.Map<DataTransferModels.Event>(dataAccessEventModel);
+        }
+        
+        public async Task<DataTransferModels.Event> GetSingleEvent(int id)
+        {
+            Entities.Event dataAccessEventModel = await _eventRepository.GetSingleEvent(id);
+            return _mapper.Map<DataTransferModels.Event>(dataAccessEventModel);
+        }
+        
+        public async Task<bool> DeleteEvent(int id)
+        {
+            bool wasRemoved = await _eventRepository.DeleteEvent(id);
+            return wasRemoved;
+        }
+    }
+}
