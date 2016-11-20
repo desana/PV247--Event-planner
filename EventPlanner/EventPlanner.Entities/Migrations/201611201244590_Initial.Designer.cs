99cbe8d84e11d1edfc709d166f15f37358be246d
Jana Morozová <morozova.jana@gmail.com>
2016-11-20 14:49:21 +0100

    New DB migration scripts

--- EventPlanner/EventPlanner.Entities/Migrations/201611201244590_Initial.Designer.cs
+++ EventPlanner/EventPlanner.Entities/Migrations/201611201244590_Initial.Designer.cs
@@ -7,13 +7,13 @@ namespace EventPlanner.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.1.3-40302")]
-    public sealed partial class DatabaseRefactoring : IMigrationMetadata
+    public sealed partial class Initial : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(DatabaseRefactoring));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(Initial));
         
         string IMigrationMetadata.Id
         {
-            get { return "201611141606304_DatabaseRefactoring"; }
+            get { return "201611201244590_Initial"; }
         }
         
         string IMigrationMetadata.Source
---
+++ EventPlanner/EventPlanner.Entities/Migrations/201611201244590_Initial.cs
@@ -0,0 +1,61 @@
+namespace EventPlanner.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Initial : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Events",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false, maxLength: 50),
+                        Description = c.String(),
+                        Link = c.String(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.TimeAtPlaces",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Time = c.DateTime(nullable: false),
+                        Votes = c.Int(nullable: false),
+                        Event_Id = c.Int(),
+                        Place_Id = c.Int(),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Events", t => t.Event_Id)
+                .ForeignKey("dbo.Places", t => t.Place_Id)
+                .Index(t => t.Event_Id)
+                .Index(t => t.Place_Id);
+            
+            CreateTable(
+                "dbo.Places",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        FourSquareId = c.String(),
+                        FourSquareLink = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.TimeAtPlaces", "Place_Id", "dbo.Places");
+            DropForeignKey("dbo.TimeAtPlaces", "Event_Id", "dbo.Events");
+            DropIndex("dbo.TimeAtPlaces", new[] { "Place_Id" });
+            DropIndex("dbo.TimeAtPlaces", new[] { "Event_Id" });
+            DropTable("dbo.Places");
+            DropTable("dbo.TimeAtPlaces");
+            DropTable("dbo.Events");
+        }
+    }
+}
