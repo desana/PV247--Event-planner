e4c0cb5b1d4632f4cfd92f45cb67dcf067c9a428
xvitovs1 <409920@mail.muni.cz>
2016-11-29 08:55:38 +0100

    Add tests for foursquare service

---
+++ EventPlanner/EventPlanner.FoursquareVenuesService.UnitTests/FoursquareTests.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Configuration;
+using System.Linq;
+using FoursquareVenuesService;
+using FoursquareVenuesService.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EventPlanner.FoursquareVenuesService.UnitTests
+{
+
+    [TestClass]
+    public class FoursquareTests
+    {
+        private FoursquareService _fsService;
+
+        [TestInitialize]
+        public void InitializeTests()
+        {
+            _fsService = new FoursquareService(new FoursquareOptions
+            {
+                ClientId = ConfigurationManager.AppSettings["clientId"],
+                ClientSecret = ConfigurationManager.AppSettings["clientSecret"]
+            });
+        }
+
+        [TestMethod]
+        public void TestSearchVenuesAsync()
+        {
+            var task = _fsService.SearchVenuesAsync("U Karla", "Brno", 10);
+            task.Wait();
+
+            var venues = task.Result.ToList();
+            Assert.IsFalse(venues.Count == 0 || venues.Count > 10);
+
+            foreach (var venue in venues)
+            {
+                if (venue.Location.City != null)
+                {
+                    Assert.AreEqual("Brno", venue.Location.City);
+                }
+            }
+        }
+
+        [TestMethod]
+        public void TestGetVenueAsync()
+        {
+            // Get id of some existing venue
+            var task = _fsService.SearchVenuesAsync("U Karla", "Brno", 10);
+            task.Wait();
+            var result = task.Result.ToList();
+            var id = result[0].Id;
+
+            // Test GetVenueAsync
+            var task2 = _fsService.GetVenueAsync(id);
+            task2.Wait();
+            var venue = task2.Result;
+
+            Assert.AreEqual(id, venue.Id);
+            Assert.AreEqual(result[0].Name, venue.Name);
+            Assert.AreEqual(result[0].Url, venue.Url);
+            Assert.AreEqual(result[0].Location.City, venue.Location.City);
+        }
+
+        [TestMethod]
+        public void TestGetVenuePhotoUrlAsync()
+        {
+            // Get id of some existing venue
+            var task = _fsService.SearchVenuesAsync("U Karla", "Brno", 10);
+            task.Wait();
+            var result = task.Result.ToList();
+            var id = result[0].Id;
+
+            // Test GetVenuePhotoUrlAsync
+            var task2 = _fsService.GetVenuePhotoUrlAsync(id);
+            task2.Wait();
+            var photo = task2.Result;
+
+            if (!String.IsNullOrEmpty(photo))
+            {
+                Assert.IsTrue(photo.Contains("http"));
+                Assert.IsTrue(photo.Contains("img"));
+            }
+
+
+        }
+    }
+}
---
+++ EventPlanner/EventPlanner.FoursquareVenuesService.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("EventPlanner.FoursquareVenuesService.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("EventPlanner.FoursquareVenuesService.UnitTests")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8749fd21-6bd4-4d95-800c-40f2fef77a7d")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
