a79ef9e05ca2d40d9ac8e4be08523f854b7a025f
xvitovs1 <409920@mail.muni.cz>
2016-12-04 20:19:22 +0100

    Get opening hours from FS

---
+++ EventPlanner/FoursquareVenuesService/Entities/Time.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoursquareVenuesService.Entities
+{
+    /// <summary>
+    /// Representation of times from opening hours of Foursquare venue (https://developer.foursquare.com/docs/responses/hours.html).
+    /// </summary>
+    public class Time
+    {
+        /// <summary>
+        /// Time from opening hours.
+        /// </summary>
+        public string RenderedTime { get; set; }
+    }
+}
