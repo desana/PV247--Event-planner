2b2ffac0df97406184149ab3759e12b51730cd32
xvitovs1 <409920@mail.muni.cz>
2016-12-05 09:43:16 +0100

    Results page: Do not show diagrams with no votes

--- EventPlanner/EventPlanner/wwwroot/js/results-chart.js
+++ EventPlanner/EventPlanner/wwwroot/js/results-chart.js
@@ -1,33 +1,34 @@
 ﻿$(function () {
-    var myChart = Highcharts.chart('container', {
-        chart: {
-            type: 'bar'
-        },
-        title: {
-            text: dataJson.eventName
-        },
-        xAxis: {
-            categories: dataJson.categories
-        },
-        yAxis: {
+    if (dataJson.categories.length === 0) {
+        $("#container").text("There are no votes for this event.");
+    } else {
+        var myChart = Highcharts.chart("container", {
+            chart: {
+                type: "bar"
+            },
             title: {
-                text: 'Number of votes'
-            }
-        },
-        series: [{
-            name: 'Votes',
-            data: dataJson.data
-        }]
-    });
-});
-
-$("#toggle").click(function () {
-    $("#toggle-div").toggle();
+                text: dataJson.eventName
+            },
+            xAxis: {
+                categories: dataJson.categories
+            },
+            yAxis: {
+                title: {
+                    text: "Number of votes"
+                }
+            },
+            series: [{
+                name: "Votes",
+                data: dataJson.data
+            }]
+        });
+    }
+    
 });
 
 $(".toggle-diagram")
     .click(function() {
         $("#toggle-div").toggle();
-        $("#arrow").toggleClass('glyphicon-menu-up');
-        $("#arrow").toggleClass('glyphicon-menu-down');
+        $("#arrow").toggleClass("glyphicon-menu-up");
+        $("#arrow").toggleClass("glyphicon-menu-down");
     });
